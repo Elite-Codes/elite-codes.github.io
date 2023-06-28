f412fd2662db2f554ad1f263009b45c4358adfca
Waseem <wakk97@gmail.com>
2023-06-28 01:37:49 +0100

    code formatted

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/hero'
 import Hero02 from './components/hero_02'
--- src/components/hero_02.jsx
+++ src/components/hero_02.jsx
@@ -1,26 +1,31 @@
-import './css/hero_02.css'
+import "./css/hero_02.css";
 
-export default function Hero02(){
-    return(
-        <section className='hero02'>
-            <div className="hero2--cover-text"> <h1>Software Developement & <span className='underline'>IT Services</span> </h1> </div>
-            <div className="cards">
-                <div className="card">
-                    <img src="/dev icon.svg" alt="" />
-                    <h4>Web design</h4>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                </div>
-                <div className="card">
-                    <img src="/dev icon.svg" alt="" />
-                    <h4>Branding</h4>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                </div>
-                <div className="card">
-                    <img src="/dev icon.svg" alt="" />
-                    <h4>Development</h4>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                </div>
-            </div>
-        </section>
-    )
+export default function Hero02() {
+  return (
+    <section className="hero02">
+      <div className="hero2--cover-text">
+        {" "}
+        <h1>
+          Software Developement & <span className="underline">IT Services</span>{" "}
+        </h1>{" "}
+      </div>
+      <div className="cards">
+        <div className="card">
+          <img src="/dev icon.svg" alt="" />
+          <h4>Web design</h4>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </div>
+        <div className="card">
+          <img src="/dev icon.svg" alt="" />
+          <h4>Branding</h4>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </div>
+        <div className="card">
+          <img src="/dev icon.svg" alt="" />
+          <h4>Development</h4>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </div>
+      </div>
+    </section>
+  );
 }
