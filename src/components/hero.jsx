5252eaf91c7b19d4d32bd7d56e259f397d1d1579
Waseem <wakk97@gmail.com>
2023-06-28 02:28:57 +0100

    content-bootstrap-resposive

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,17 +1,20 @@
-import './css/hero.css'
+import "./css/hero.css";
 
-export default function Hero(){
-    return(
-        <section className='hero d-flex flex-column justify-content-center align-items-start'>
-            <div className="hero--text">
-
-            <h1 className='primary-heading'> We Provide You Custom <br />
-            Softwares And IT Services</h1>
-            <p className='secondary-heading'>We are a full service technology and digital solutions company with over 20 years of experience in industry.</p>
-
-            </div>
-            <button className='btn btn--primary--animated'>Learn More</button>
-
-        </section>
-    )
+export default function Hero() {
+  return (
+    <section className="hero d-flex flex-column justify-content-center align-items-start">
+      <div className="hero--text">
+        <h1 className="primary-heading">
+          {" "}
+          We Provide You Custom <br />
+          Softwares And IT Services
+        </h1>
+        <p className="secondary-heading">
+          We are a full service technology and digital solutions company with
+          over 20 years of experience in industry.
+        </p>
+      </div>
+      <button className="btn btn--primary--animated">Learn More</button>
+    </section>
+  );
 }
--- src/components/hero_02.jsx
+++ src/components/hero_02.jsx
@@ -2,12 +2,13 @@ import "./css/hero_02.css";
 
 export default function Hero02() {
   return (
-    <section className="hero02">
-      <div className="hero2--cover-text">
+    <section className="bg-white">
+      {/* <section className="hero02"> */}
+      {/* <div className="hero2--cover-text">
         {" "}
         <h1>
-          Software Developement & <span className="underline">IT Services</span>{" "}
-        </h1>{" "}
+          Software Developement & <span className="underline">IT Services</span>
+        </h1>
       </div>
       <div className="cards">
         <div className="card">
@@ -24,6 +25,33 @@ export default function Hero02() {
           <img src="/dev icon.svg" alt="" />
           <h4>Development</h4>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </div>
+      </div>  */}
+      <div>
+        <div className="row justify-content-around">
+          <div className="col-md-4 blk text-center">
+            <div>
+              <img src="/dev icon.svg" alt="" />
+            </div>
+            <div>
+              <h4>Web design</h4>
+            </div>
+            <div>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </div>
+          </div>
+          <div className="col-md-4 blk text-center">
+            {" "}
+            <img src="/dev icon.svg" alt="" />
+            <h4>Web design</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </div>
+          <div className="col-md-4 blk text-center">
+            {" "}
+            <img src="/dev icon.svg" alt="" />
+            <h4>Web design</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </div>
         </div>
       </div>
     </section>
