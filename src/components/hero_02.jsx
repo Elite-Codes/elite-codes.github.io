a5e4648d17cf0c8d78a455986529cd1dc94f7ea2
Shirazjaf <ishirazjaf@gmail.com>
2023-07-03 20:15:23 +0530

    Fixed Component Names

--- src/components/hero_02.jsx
+++ src/components/hero_02.jsx
@@ -8,34 +8,34 @@ export default function Hero02(){
             <div className="row justify-content-around">
                 <div className="feat col-md-4">
                     <img src="/icon_design.svg" alt="" />
-                    <h4>Web design</h4>
+                    <h4 className='feat-title'>Web design</h4>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                 </div>
                 <div className="feat col-md-4">
                     <img src="/icon_branding.svg" alt="" />
-                    <h4>Branding</h4>
+                    <h4 className='feat-title'>Branding</h4>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                 </div>
                 <div className="feat col-md-4">
                     <img src="/icon_webdev.svg" alt="" />
-                    <h4>Development</h4>
+                    <h4 className='feat-title'>Development</h4>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                 </div>
             </div>
             <div className="row justify-content-around">
                 <div className="feat col-md-4">
                     <img src="/icon_marketing.svg" alt="" />
-                    <h4>Marketing</h4>
+                    <h4 className='feat-title'>Marketing</h4>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                 </div>
                 <div className="feat col-md-4">
                     <img src="/icon_bisconsul.svg" alt="" />
-                    <h4>Strategy</h4>
+                    <h4 className='feat-title'>Strategy</h4>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                 </div>
                 <div className="feat col-md-4">
                     <img src="/icon_trp.svg" alt="" />
-                    <h4>ERP</h4>
+                    <h4 className='feat-title'>ERP</h4>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit lorem10</p>
                 </div>
             </div>
