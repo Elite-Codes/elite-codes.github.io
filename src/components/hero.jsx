1b8d5f55973f2962167b8d6edffc87c2c44c57b2
Shirazjaf <ishirazjaf@gmail.com>
2023-07-03 23:43:30 +0530

    Added responsiveness to landing page

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -2,7 +2,7 @@ import './css/hero.css'
 
 export default function Hero(){
     return(
-        <section className='hero d-flex flex-column justify-content-center align-items-start'>
+        <section className='hero d-flex flex-column justify-content-center'>
             <div className="hero--text">
 
             <h1 className='primary-heading'> We Provide You Custom <br />
@@ -10,7 +10,10 @@ export default function Hero(){
             <p className='secondary-heading'>We are a full service technology and digital solutions company with over 20 years of experience in industry.</p>
 
             </div>
-            <button className='btn btn--primary--animated'>Learn More</button>
+            <div className='button'>
+            <a href="#" class="btn btn--primary--animated">Learn More</a>
+            </div>
+            
 
         </section>
     )
