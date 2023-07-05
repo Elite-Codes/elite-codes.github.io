574eed61eda98796fe3c812676e1b5091ad6730b
Shirazjaf <ishirazjaf@gmail.com>
2023-07-05 16:37:08 +0530

    Hero04 added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar'
 import Hero from './components/hero'
 import Hero02 from './components/hero_02'
 import Hero03 from './components/hero_03'
+import Hero04 from './components/hero_04'
 import Footer from './components/footer'
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Hero />
       <Hero02 />
       <Hero03 />
+      <Hero04 />
       <Footer />
     </div>
   )
--- src/components/hero_03.jsx
+++ src/components/hero_03.jsx
@@ -5,21 +5,21 @@ export default function Hero03() {
         <section className="hero03">
             <div className="container">
                 <div className="row">
-                    <div className="col-md-6">
-                        <h1>Keep Your Business Safe & <br />Ensure  high Availbility.</h1>
-                        <img src="./Rectangle 21.png" alt="" />
-                        </div>
                     <div className="hero03--rhs col-md-6">
                         <div className="hero03--rhs">
-                            <ul>
-
-                                <li><img src="./checkmark.png" alt="" /> Delivered more than 4k client interaction</li>
-                                <li><img src="./checkmark.png" alt="" /> Provided by experts to help challenge activities</li>
-                                <li><img src="./checkmark.png" alt="" /> Completed with peer perspectives and advice</li>
+                        <h1>Keep Your Business Safe & <br />Ensure  high Availbility.</h1>
+                        <p className='title--describe'>Pick one of our stock themes, or create your custom theme with the most advanced theme editor on any online survey building tool.</p>
+                            <ul className='p-0'>
+                                <li><img src="./svgexport-7.svg" alt="" /> Delivered more than 4k client interaction</li>
+                                <li><img src="./svgexport-7.svg" alt="" /> Provided by experts to help challenge activities</li>
+                                <li><img src="./svgexport-7.svg" alt="" /> Completed with peer perspectives and advice</li>
                             </ul>
-                            <button className="btn">More About us</button>
+                            <button className="btn mx-0 hero03--btn">Explore More &rarr;</button>
                         </div>
                     </div>
+                    <div className="col-md-6">
+                        <img src="./dashboard.webp" alt="" />
+                        </div>
                 </div>
             </div>
         </section>
