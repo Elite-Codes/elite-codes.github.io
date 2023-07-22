e883cf60f10a7a489922bc1c854a2829e69bed7f
Shirazjaf <ishirazjaf@gmail.com>
2023-07-22 17:12:31 +0530

    feat: why choose us page

---
+++ src/components/feature.jsx
@@ -0,0 +1,38 @@
+import './css/feature.css'
+
+export default function Feature(){
+    return(
+        <section className="feature-section">
+            <div className="container-fluid feat-contain">
+                <div className="container contents-contain">
+                    <div className="contents-contain-text">
+                    <h1>Codeqube provides everything you need to <br />build a beautiful website for your SaaS</h1>
+                    <p>Receive dividends bull investment mutual funds rise federal funds strategy <br /> substantially treasury municipal public index funds.</p>                  
+                    </div>
+                    <div className="row justify-content-around">
+                        <div className="col-md-4 feature-box">
+                            <img src="feature-1.webp" alt="" />
+                            <h4>Exceptional team</h4>
+                            <p>Debt bills lucrative capitalization municipal bonds government 401k bear bull credit corporate bonds called NYSE finance.</p>
+                        </div>
+                        <div className="col-md-4 feature-box">
+                            <img src="feature-2.webp" alt="" />
+                            <h4>Flexible payment</h4>
+                            <p>Debt bills lucrative capitalization municipal bonds government 401k bear bull credit corporate bonds called NYSE finance.</p>
+                        </div>
+                        <div className="col-md-4 feature-box">
+                            <img src="feature-1.webp" alt="" />
+                            <h4>Prompt service</h4>
+                            <p>Debt bills lucrative capitalization municipal bonds government 401k bear bull credit corporate bonds called NYSE finance.</p>
+                        </div>
+                        <div className="col-md-4 feature-box">
+                            <img src="feature-1.webp" alt="" />
+                            <h4>Top notch software</h4>
+                            <p>Debt bills lucrative capitalization municipal bonds government 401k bear bull credit corporate bonds called NYSE finance.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,12 +6,15 @@ import Hero04 from "../components/hero_04";
 import Footer from "../components/footer";
 import Aboutus from "../components/aboutus";
 import Form from "../components/form";
+import Feature from "../components/feature";
+
 export default function Home() {
   return (
     <div>
       <Navbar />
       <Hero />
       <Hero02 />
+      <Feature />
       <Hero03 />
       <Aboutus />
       <Hero04 />
